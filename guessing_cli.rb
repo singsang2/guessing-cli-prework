cfc14b42388529333e134f7e62f3c3903191bb1a
Sung Bae <singsang2@gmail.com>
2018-10-14 23:31:20 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,27 @@
 # Code your solution here!
+def run_guessing_game
+  
+  
+end
+
+def get_input
+  puts "Input:"
+  user_input = gets.strip.to_i
+  
+end
+
+def get_random_number
+  @number = rand
+end
+
+def guessed_right?
+  @number == @guess
+end
+
+def win
+  puts "You guessed the correct number!"
+end
+
+def lose
+  puts "The computer guessed #{@number}."
+end
